af62deaf9a70d7530eb6ec7da41cde6add36385e
MahnoorMukhtar <mahnoormukhtar519@gmail.com>
2023-07-27 16:48:48 +0500

    Commiting

---
+++ README.md
@@ -0,0 +1,21 @@
+# Basketball Scoreboard JavaScript Project
+
+### Game Screenshot
+
+![](images/Screenshot%20(276).png)
+
+### Languages and Tools Used
+
+- HTML5
+- CSS3
+- JavaScript
+- VS Code
+
+### Acknowledgements
+
+- [Scrimba](https://scrimba.com)
+
+## Author
+
+- Github - [@MahnoorMukhtar](https://github.com/MahnoorMukhtar)
+- LinkedIn - [Mahnoor Mukhtar](https://www.linkedin.com/in/mahnoor-mukhtar-9606b7244/)
---
+++ index.js
@@ -0,0 +1,37 @@
+let hAddEl=document.getElementById("h-score")
+let gAddEl=document.getElementById("g-score")
+
+function add1()
+{
+    hAddEl.textContent++;
+}
+function Hadd2()
+{
+    hAddEl.textContent=Number(hAddEl.textContent)+2;
+}
+function Hadd3()
+{
+    hAddEl.textContent=Number(hAddEl.textContent)+3;
+}
+function Hreset()
+{
+    hAddEl.textContent=0;
+}
+function Gadd1()
+{
+    gAddEl.textContent++;
+}
+function Gadd2()
+{
+    gAddEl.textContent=Number(gAddEl.textContent)+2;
+}
+function Gadd3()
+{
+    gAddEl.textContent=Number(gAddEl.textContent)+3;
+}
+function Greset()
+{
+    gAddEl.textContent=0;
+}
+
+
